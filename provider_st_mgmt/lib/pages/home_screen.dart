3834e3a2204b046ee9ee40d1318ba7fb927b782e
rn-byte <ramthakur977+github@gmail.com>
2024-06-22 23:36:02 +0545

    This is 2nd Commit

--- provider_st_mgmt/lib/pages/home_screen.dart
+++ provider_st_mgmt/lib/pages/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,13 +10,46 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  var num = 1;
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(
+      const Duration(seconds: 1),
+      (timer) {
+        num++;
+        setState(() {});
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    debugPrint('build');
     return Scaffold(
       body: Container(
+        margin: const EdgeInsets.all(20),
         child: Column(
-          children: [],
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Text(
+                '${DateTime.now().hour}:${DateTime.now().minute}:${DateTime.now().second}\n$num',
+                style:
+                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          num++;
+          print(num.toString());
+          setState(() {});
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
