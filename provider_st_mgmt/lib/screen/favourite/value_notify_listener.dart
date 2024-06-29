7925e2457f321c320c8afdd487c559706b6371f3
rn-byte <ramthakur977+github@gmail.com>
2024-06-29 14:24:11 +0545

    StatelessWidget as StatefulWidget eg. 2 added

--- provider_st_mgmt/lib/screen/favourite/value_notify_listener.dart
+++ provider_st_mgmt/lib/screen/favourite/value_notify_listener.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 class ValueNotifyListenerScreen extends StatelessWidget {
   ValueNotifyListenerScreen({super.key});
 
-  ValueNotifier<int> _counter = ValueNotifier<int>(0);
+  final ValueNotifier<int> _counter = ValueNotifier<int>(0);
+  ValueNotifier<bool> toogle = ValueNotifier<bool>(true);
 
   @override
   Widget build(BuildContext context) {
@@ -14,16 +15,56 @@ class ValueNotifyListenerScreen extends StatelessWidget {
         title: const Text("Provider in StatelessWidget"),
         centerTitle: true,
       ),
-      body: Center(
-        child: ValueListenableBuilder(
-          valueListenable: _counter,
-          builder: (context, value, child) {
-            return Text(
-              _counter.value.toString(),
-              style: const TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-            );
-          },
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          const Text(
+            'Example 2',
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+          ),
+          Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ValueListenableBuilder(
+                valueListenable: toogle,
+                builder: (context, value, child) {
+                  return TextFormField(
+                    obscureText: toogle.value,
+                    decoration: InputDecoration(
+                        hintText: 'Enter Password',
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            toogle.value = !toogle.value;
+                          },
+                          child: Icon(
+                            toogle.value
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                          ),
+                        ),
+                        border: const OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30)))),
+                  );
+                },
+              )),
+          const Divider(),
+          const Text(
+            'Example 1',
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+          ),
+          Center(
+            child: ValueListenableBuilder(
+              valueListenable: _counter,
+              builder: (context, value, child) {
+                return Text(
+                  _counter.value.toString(),
+                  style: const TextStyle(
+                      fontSize: 50, fontWeight: FontWeight.bold),
+                );
+              },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
