0cc87b21cf66bbb3f6a0444b1a1ea2724eb7a22c
rn-byte <ramthakur977+github@gmail.com>
2024-07-04 23:26:00 +0545

    alert dialog and bottomSheet

--- getx_st_mgmt/lib/Views/home_view.dart
+++ getx_st_mgmt/lib/Views/home_view.dart
@@ -17,14 +17,76 @@ class _HomeViewState extends State<HomeView> {
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: SafeArea(
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text('This is GetX Tutorial'),
+            const Text('This is GetX Tutorial'),
+            Card(
+              child: ListTile(
+                title: const Text('This is GetX Dialog.'),
+                subtitle: const Text('Click To Get Alert Dialog'),
+                onTap: () => Get.defaultDialog(
+                  title: 'Alert!',
+                  titlePadding: const EdgeInsets.only(top: 20),
+                  middleText: 'Are you sure you want to Delete?',
+                  contentPadding: const EdgeInsets.all(20),
+                  confirm: TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Ok')),
+                  cancel: TextButton(
+                      onPressed: () {
+                        Get.back();
+                      },
+                      child: const Text('Cancel')),
+                  //content: ,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Card(
+              child: ListTile(
+                title: const Text('GetX BottomSheet'),
+                subtitle: const Text('GetX BottomSheet With Click'),
+                onTap: () {
+                  Get.bottomSheet(SizedBox(
+                    child: Column(
+                      children: [
+                        ListTile(
+                          leading: const Icon(Icons.light_mode),
+                          title: const Text('Light Mode'),
+                          onTap: () {
+                            Get.changeThemeMode(ThemeMode.light);
+                          },
+                        ),
+                        ListTile(
+                          leading: const Icon(Icons.dark_mode),
+                          title: const Text('Dark Mode'),
+                          onTap: () {
+                            Get.changeThemeMode(ThemeMode.dark);
+                          },
+                        ),
+                        ListTile(
+                          leading: const Icon(Icons.system_security_update),
+                          title: const Text('System Mode'),
+                          onTap: () {
+                            Get.changeThemeMode(ThemeMode.system);
+                          },
+                        ),
+                      ],
+                    ),
+                  ));
+                },
+              ),
+            )
           ],
-        ),
+        )),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
