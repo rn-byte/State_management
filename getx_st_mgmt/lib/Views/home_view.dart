3cb46b2aedc52d9a98cb7d01918d51abd43089a1
rn-byte <ramthakur977+github@gmail.com>
2024-07-04 21:30:18 +0545

    some problem solved

--- getx_st_mgmt/lib/Views/home_view.dart
+++ getx_st_mgmt/lib/Views/home_view.dart
@@ -10,6 +10,11 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("GetX"),
+      ),
+      body: Container(),
+    );
   }
 }
--- getx_st_mgmt/lib/main.dart
+++ getx_st_mgmt/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'Views/home_view.dart';
 
 void main() {
