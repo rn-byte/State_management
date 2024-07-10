f8067b42cff0767ec838e227a6e5b2aa6ae731c2
rn-byte <ramthakur977+github@gmail.com>
2024-07-10 23:43:47 +0545

    fav_app repository added

--- bloc_st_mgmt/lib/repository/fav_repository.dart
+++ bloc_st_mgmt/lib/repository/fav_repository.dart
@@ -1,1 +1,12 @@
-class FavRepository {}
+import 'package:bloc_st_mgmt/models/fav_app/fav_items_model.dart';
+
+class FavRepository {
+  Future<List<FavItemsModel>> fetchItem() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return List.of(_generateList(10));
+  }
+
+  List<FavItemsModel> _generateList(int length) {
+    return List.generate(length, (index) => FavItemsModel(id: index.toString(), value: 'Item : $index'),)
+  }
+}
