d2ae0b4eee47a0f1ccaed73db5a362692d3ac85b
rn-byte <ramthakur977+github@gmail.com>
2024-07-10 23:23:20 +0545

    bloc event added

---
+++ bloc_st_mgmt/lib/bloc/fav_app/fav_app_events.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+
+class FavAppEvents extends Equatable {
+  const FavAppEvents();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class FetchFavAppList extends FavAppEvents {}
