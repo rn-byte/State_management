8a07481292d246a52042b440511eb97c05873412
rn-byte <ramthakur977+github@gmail.com>
2024-07-16 18:04:08 +0545

    some dependency added

--- blc_st_mgmt_clean_archit/lib/main.dart
+++ blc_st_mgmt_clean_archit/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screen/home_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,11 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: const HomeScreen(),
     );
   }
 }
