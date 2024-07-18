5b371019f33eee67ee300d9d6ec68940da6cf85b
rn-byte <ramthakur977+github@gmail.com>
2024-07-18 23:09:47 +0545

    enum login repo and app url added

--- blc_st_mgmt_clean_archit/lib/bloc/login/login_states.dart
+++ blc_st_mgmt_clean_archit/lib/bloc/login/login_states.dart
@@ -3,19 +3,30 @@ part of 'login_bloc.dart';
 class LoginStates extends Equatable {
   final String email;
   final String password;
+  final Status status;
+  final String error;
 
   const LoginStates({
     this.email = '',
     this.password = '',
+    this.status = Status.initial,
+    this.error = '',
   });
 
-  LoginStates copyWith({String? email, String? password}) {
+  LoginStates copyWith({
+    String? email,
+    String? password,
+    Status? status,
+    String? error,
+  }) {
     return LoginStates(
       email: email ?? this.email,
       password: password ?? this.password,
+      status: status ?? this.status,
+      error: error ?? this.error,
     );
   }
 
   @override
-  List<Object?> get props => [email, password];
+  List<Object?> get props => [email, password, status, error];
 }
---
+++ blc_st_mgmt_clean_archit/lib/config/app_url.dart
@@ -0,0 +1,10 @@
+class AppUrl {
+  static const String baseUrl = 'https://reqres.in/';
+
+  static const String loginUrlEndpoint = '${baseUrl}api/login';
+
+  static const String registerUrlEndpoint = '${baseUrl}api/register';
+
+  static const String userListApiUrl =
+      'https://webhook.site/8ce6b95b-a2de-4ea6-9411-ed77256566b0';
+}
