e2b571b52b3f221d2563444e8e25660156f9f498
rn-byte <ramthakur977+github@gmail.com>
2024-07-18 10:08:51 +0545

    Freezed model added

--- blc_st_mgmt_clean_archit/lib/models/user/user_model.dart
+++ blc_st_mgmt_clean_archit/lib/models/user/user_model.dart
@@ -1,1 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'user_model.freezed.dart';
+part 'user_model.g.dart';
 
+@freezed
+class UserModel with _$UserModel {
+  factory UserModel({
+    @Default('') @JsonKey(name: 'token') String token,
+    @Default('') @JsonKey(name: 'error') String error,
+  }) = _UserModel;
+
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      _$UserModelFromJson(json);
+}
