f498b7bdd445ab938bfdc92b876eefc5ae67ee33
rn-byte <ramthakur977+github@gmail.com>
2024-07-21 12:42:49 +0545

    API Response Class added

---
+++ blc_st_mgmt_clean_archit/lib/data/response/api_response.dart
@@ -0,0 +1,18 @@
+import '../../utils/enum/enums.dart';
+
+class ApiResponse<T> {
+  Status? status;
+  T? data;
+  String? message;
+
+  ApiResponse({this.status, this.data, this.message});
+
+  ApiResponse.loading() : status = Status.loading;
+  ApiResponse.completed(this.data) : status = Status.success;
+  ApiResponse.error(this.message) : status = Status.error;
+
+  @override
+  String toString() {
+    return 'Status : $status \n Message : $message \n Data : $data';
+  }
+}
